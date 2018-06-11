4ef74a8f8f013bdc35c73416b4bc30530115cc14
RICHARD APULIBA ATAWINE <Richard@Lord.RICH>
2018-06-11 06:35:20 +0000

    first function in php

---
+++ function.php
@@ -0,0 +1,19 @@
+<html>
+   
+   <head>
+      <title>Writing PHP Function which returns value</title>
+   </head>
+   
+   <body>
+      <?php
+  function myfunction( $string ) {
+  return "$string!";
+}
+
+echo myfunction( "Programming is fun and rewaarding" );
+
+
+?>
+      
+   </body>
+</html>
